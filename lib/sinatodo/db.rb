a9d2b56a47cc9bf326773063c3aa1f34270afff2
yasugahira0810 <hourou_freak@yahoo.co.jp>
2015-10-27 08:03:00 +0000

    modify indent
    lib/sinatodo.rbで相対パスでrequireしている部分を、相対パスにせずにrequireできないか試したが、駄目だった。
    config/database.ymlが正しいか確認しろというエラーが出るので、sinatlistのようにdatabase.ymlを作成したりもしたが、駄目だった。
    	modified:   lib/sinatodo/db.rb

--- lib/sinatodo/db.rb
+++ lib/sinatodo/db.rb
@@ -13,36 +13,36 @@ module Sinatodo
       database_path = File.join(ENV['HOME'], '.sinatodo', 'sinatodo.db')
       connect_database database_path
       create_table_if_not_exists database_path
+    end
+
+    def self.connect_database(path)
+      spec = {adapter: 'sqlite3', database: path}
+      ActiveRecord::Base.establish_connection spec
+    end
+
+    def self.create_table_if_not_exists(path)
+      create_database_path path
+      connection = ActiveRecord::Base.connection
+
+      return if connection.table_exists?(:tasks)
+
+      connection.create_table :tasks do |t|
+        t.column :name, :string, null: false
+        t.column :content, :text, null: false
+        t.column :status, :integer, default: 0, null: false
+        t.timestamps null: false
       end
 
-      def self.connect_database(path)
-        spec = {adapter: 'sqlite3', database: path}
-        ActiveRecord::Base.establish_connection spec
-      end
-
-      def self.create_table_if_not_exists(path)
-        create_database_path path
-        connection = ActiveRecord::Base.connection
-
-        return if connection.table_exists?(:tasks)
-
-        connection.create_table :tasks do |t|
-          t.column :name, :string, null: false
-          t.column :content, :text, null: false
-          t.column :status, :integer, default: 0, null: false
-          t.timestamps null: false
-        end
-
-        connection.add_index :tasks, :status
-        connection.add_index :tasks, :created_at
-      end
-        
-      def self.create_database_path(path)
-        FileUtils.mkdir_p File.dirname(path)
-      end
+      connection.add_index :tasks, :status
+      connection.add_index :tasks, :created_at
+    end
+      
+    def self.create_database_path(path)
+      FileUtils.mkdir_p File.dirname(path)
+    end
    
-      # 公開不要なメソッドの可視性をprivateにする
-      private_class_method :connect_database, :create_table_if_not_exists, :create_database_path
+    # 公開不要なメソッドの可視性をprivateにする
+    private_class_method :connect_database, :create_table_if_not_exists, :create_database_path
   end
 
 end
