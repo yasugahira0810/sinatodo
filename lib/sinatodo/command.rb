2425f32b85ba69bd5b49cc39e9f2a41254bb91f5
yasugahira0810 <hourou_freak@yahoo.co.jp>
2015-10-29 16:09:12 +0000

    implement CRUD

--- lib/sinatodo/command.rb
+++ lib/sinatodo/command.rb
@@ -1,8 +1,44 @@
 module Sinatodo
 
-# コマンドラインベースの処理を行うクラス
-# @author yasugahira0810
-class Command
-end
+	class Command
+  	# コマンドラインベースの処理を行うクラス
+  	# # @author yasugahira0810
+  	# class Command	
+  	def execute
+  		DB.prepare
+    end
+    
+    def create_task(name, content)
+  		# タスク作成時のstatusはデフォルト値が使われNOT_YETとなる
+  		Task.create!(name: name, content: content).reload
+		end
+
+		def delete_task(id)
+			task = Task.find(id)
+			task.destroy
+		end
+
+		def update_task(id, attributes)
+			if status_name = attributes[:status]
+				attributes[:status] = Task::STATUS.fetch(status_name.upcase)
+			end
+
+			task = Task.find(id)
+			task.update_attributes! attributes
+			task.reload
+		end
+
+		def find_tasks(status_name)
+			all_tasks = Task.order('created_at DESC')
+
+			if status_name
+				status = Task::STATUS.fetch(status_name.upcase)
+				all_tasks.status_is(status)
+			else
+				all_tasks
+			end
+		end
+
+  end
 
 end
