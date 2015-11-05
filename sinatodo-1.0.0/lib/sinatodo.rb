21f0f705ce00dad0fa7102a0a6d7d32a325874a9
yasugahira0810 <hourou_freak@yahoo.co.jp>
2015-11-05 14:23:30 +0000

    create gem

--- sinatodo-1.0.0/lib/sinatodo.rb
+++
@@ -1,9 +0,0 @@
-require "sinatodo/command"
-require "sinatodo/command/options"
-require "sinatodo/db"
-require "sinatodo/task"
-require "sinatodo/version"
-
-#module Sinatodo
-  # Your code goes here...
-#end
--- sinatodo-1.0.0/lib/sinatodo/version.rb
+++
@@ -1,3 +0,0 @@
-module Sinatodo
-  VERSION = "1.0.0"
-end
