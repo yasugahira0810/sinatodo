72b681a83bad41e6d5f5efb9a133749cd56832aa
yasugahira0810 <hourou_freak@yahoo.co.jp>
2015-11-08 13:03:54 +0000

    add development settings

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+group :development do
+	gem 'sinatra-reloader'
+	gem 'thin'
+end
+
 # Specify your gem's dependencies in sinatodo.gemspec
 gemspec
 gem 'sqlite3'
--- lib/application.rb
+++ lib/application.rb
@@ -1,11 +1,28 @@
 require 'sinatra/base'
 require 'haml'
+require 'sinatodo/db'
+require 'sinatodo/task'
 
 module Sinatodo
 
 	class Application < Sinatra::Base
 
+		configure do
+			DB.prepare
+		end
+
+		configure :development do
+			require 'sinatra/reloader'
+			register Sinatra::Reloader
+		end
+
 		get '/' do
+			redirect '/tasks'
+		end
+
+		get '/tasks' do
+			@tasks = Task.all
+			
 			haml :index
 		end
 
